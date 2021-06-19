ead03257f85738a842479e2db8d6a5e10675fff1
Manasi Belekar <manasibelekar@yahoo.com>
2021-06-19 17:34:49 +1000

    Add custom matcher

--- spec/lib/loader_spec.rb
+++ spec/lib/loader_spec.rb
@@ -2,10 +2,10 @@
 
 require './lib/loader'
 require 'support/custom_matchers'
-require 'support/loader_spec_helper'
+require 'support/spec_helpers/data_spec_helper'
 
 describe Loader do
-  include LoaderSpecHelper
+  include DataSpecHelper
 
   subject(:loader) { described_class.new(config, parser) }
 
@@ -48,7 +48,7 @@ describe Loader do
         end
 
         it 'loads users data from multiple files in expected format' do
-          expect(loader).to load_files_for_type(parser, file_path, kwargs)
+          is_expected.to load_files_for_type(parser, file_path, kwargs)
         end
       end
 
@@ -63,7 +63,7 @@ describe Loader do
         end
 
         it 'loads tickets data from multiple files in expected format' do
-          expect(loader).to load_files_for_type(parser, file_path, kwargs)
+          is_expected.to load_files_for_type(parser, file_path, kwargs)
         end
       end
     end
